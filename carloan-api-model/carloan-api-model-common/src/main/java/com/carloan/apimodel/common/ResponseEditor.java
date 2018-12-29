d6d57fad9e6ac4da03c83611d26a3417e2b53dcf
merciqiao <qiaolixue@yingu.com>
2018-12-29 20:53:50 +0800

    1.添加图片上传

---
+++ carloan-api-model/carloan-api-model-common/src/main/java/com/carloan/apimodel/common/ResponseEditor.java
@@ -0,0 +1,12 @@
+package com.carloan.apimodel.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseEditor {
+
+    public String uploaded;
+    public String url;
+}
