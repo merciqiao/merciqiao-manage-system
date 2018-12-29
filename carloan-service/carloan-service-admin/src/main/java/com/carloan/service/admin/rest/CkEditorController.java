e6234207474f8e3d40188b6dea87ab9481fd6d4f
merciqiao <812858143@qq.com>
2018-12-29 23:58:41 +0800

    1.添加图片上传接口

--- carloan-service/carloan-service-admin/src/main/java/com/carloan/service/admin/rest/CkEditorController.java
+++ carloan-service/carloan-service-admin/src/main/java/com/carloan/service/admin/rest/CkEditorController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -13,87 +14,61 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
 
 @RestController
 @Slf4j
 @RequestMapping("/img-api")
 public class CkEditorController {
-    // 图片类型
-    private static List<String> fileTypes = new ArrayList<String>();
-
-    static {
-        fileTypes.add(".jpg");
-        fileTypes.add(".jpeg");
-        fileTypes.add(".bmp");
-        fileTypes.add(".gif");
-        fileTypes.add(".png");
-    }
 
 
+    private static final String CK_IMAGE_PATH = File.separator + "uploadImage";
     @RequestMapping(value = "/upload",method = RequestMethod.POST)
     @ApiOperation(value = "上传图片", notes = "返回结果,SUCCESS:200,FAILED:500", httpMethod = "POST")
-    public ResponseEditor upload(HttpServletRequest request, HttpServletResponse response)throws Exception{
-        ResponseEditor result=new ResponseEditor();
-        try{
-            // 创建一个通用的多部分解析器
-            CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession()
-                    .getServletContext());
-            // 图片名称
-            String fileName = null;
-            // 判断 request 是否有文件上传,即多部分请求
-            if (multipartResolver.isMultipart(request)) {
-                // 转换成多部分request
-                MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
-                // 取得request中的所有文件名
-                Iterator<String> iter = multiRequest.getFileNames();
-                while (iter.hasNext()) {
-                    // 记录上传过程起始时的时间，用来计算上传时间
-                    // int pre = (int) System.currentTimeMillis();
-                    // 取得上传文件
-                    MultipartFile file = multiRequest.getFile(iter.next());
-                    if (file != null) {
-                        // 取得当前上传文件的文件名称
-                        String myFileName = file.getOriginalFilename();
-                        // 如果名称不为“”,说明该文件存在，否则说明该文件不存在
-                        if (myFileName.trim() != "") {
-                            // 获得图片的原始名称
-                            String originalFilename = file.getOriginalFilename();
-                            // 获得图片后缀名称,如果后缀不为图片格式，则不上传
-                            String suffix = originalFilename.substring(originalFilename.lastIndexOf(".")).toLowerCase();
-                            if (!fileTypes.contains(suffix)) {
-                                continue;
-                            }
-                            // 获得上传路径的绝对路径地址(/upload)-->
-                            String realPath = request.getSession().getServletContext().getRealPath("/" + "");
-                            System.out.println(realPath);
-                            // 如果路径不存在，则创建该路径
-                            File realPathDirectory = new File(realPath);
-                            if (realPathDirectory == null || !realPathDirectory.exists()) {
-                                realPathDirectory.mkdirs();
-                            }
-                            // 重命名上传后的文件名 111112323.jpg
-                            fileName = Math.random() + suffix;
-                            // 定义上传路径 .../upload/111112323.jpg
-                            File uploadFile = new File(realPathDirectory + "\\" + fileName);
-                            System.out.println(uploadFile);
-                            file.transferTo(uploadFile);
-                        }
-                    }
-                    // 记录上传该文件后的时间
-                    // int finaltime = (int) System.currentTimeMillis();
-                    // System.out.println(finaltime - pre);
-                }
-            }
-            result.setUploaded("1");
-            result.setUrl("www.baidu.com");
-            return result;
-        }catch (Exception ex) {
-            log.error(ex.getMessage(), ex);
-            return result;
+    public Map<String, String> upload(@RequestPart("upload") MultipartFile file, HttpServletRequest request)throws Exception{
+        if(file==null || "".equals(file.getOriginalFilename().trim())) {
+            return generateResult(false, "#");
         }
+        String originalName = file.getOriginalFilename();
+        // generate file name
+        String localFileName = System.currentTimeMillis() + "-" + originalName;
+        // get project path
+        String projectRealPath = request.getSession().getServletContext().getRealPath("");
+        // get the real path to store received images
+        String realPath = projectRealPath + CK_IMAGE_PATH;
+        File imageDir = new File(realPath);
+        if(!imageDir.exists()) {
+            imageDir.mkdirs();
+        }
+
+        String localFilePath = realPath + File.separator + localFileName;
+        try {
+            file.transferTo(new File(localFilePath));
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+            // log here
+        } catch (IOException e) {
+            e.printStackTrace();
+            // log here
+        }
+        String imageContextPath = "/api/"+request.getContextPath() + "/uploadImage" + "/" + localFileName;
+        // log here +
+        System.out.println("received file original name: " + originalName);
+        System.out.println("stored local file name: " + localFileName);
+        System.out.println("file stored path: " + localFilePath);
+        System.out.println("returned url: " + imageContextPath);
+        // log here -
+        return generateResult(true, imageContextPath);
     }
+
+    private Map<String, String> generateResult(boolean uploaded, String relativeUrl){
+        Map<String, String> result = new HashMap<String, String>();
+        result.put("uploaded", uploaded + "");
+        result.put("url", relativeUrl);
+
+        return result;
+    }
+
 
 }
